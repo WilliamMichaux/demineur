a13bbb3d59c6dfaf62dc8b9e36ee3da1be507fd0
William <william.michaux@gmail.com>
2018-01-29 20:57:58 +0100

    Changement de stratégie

--- js/demineur.js
+++ js/demineur.js
@@ -1,8 +1,7 @@
 //On crée l'objet case qui sera dans chaque cellule du tableau pour stocker les infos de la case
-var Case = {
-    element: "",
-    nbBombes: 0,
-    display: false,
+function Case(nbBombes, flag) {
+    this.nbBombesVoisines = nbBombes;
+    this.flag = flag;
 }
 //On récupère la difficulté et on génère une carte en fonction
 var formElt = document.getElementById("difficulte");
@@ -30,44 +29,4 @@ formElt.addEventListener("click", function (e) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-
-//On génère la carte
-function initTab(tailleCarte, nbBombes) {
-    //On créé un tableau a deux dimensions de taille n
-    var grille = [];
-    var colonne = [];
-    
-    for (var i = 0; i < tailleCarte; i++) {
-        colonne[i] = Object.create(Case);
-    }
-    
-    for (var i = 0; i < tailleCarte; i++) {
-        grille.push(colonne);
-    }
-    
-    console.log(grille[0][0].nbBombes)
-    console.log(grille[4][0].nbBombes)
-    console.log(grille[0][2].nbBombes)
-    console.log(grille[5][4].nbBombes)
-    
-    var iBombes = nbBombes;
-    /*while (iBombes > 0) {
-        var randAbs = getRandomInt(tailleCarte -1);
-        var randOrd = getRandomInt(tailleCarte -1);
-    
-        if (grille[randAbs][randOrd].nbBombes != -1){ 
-            grille[randAbs][randOrd].element ="rien";
-            grille[randAbs][randOrd].nbBombes = -1;
-            iBombes--;
-        }
-    }
-    
-    for (var iAbs; iAbs < tailleCarte; iAbs++) {
-        for (var iOrd; iOrd < tailleCarte; iOrd++) {
-            console.log(grille[iAbs][iOrd].nbBombes);
-        }
-    }*/
-
-};
-
 
