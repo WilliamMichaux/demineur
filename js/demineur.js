7f77c5b842146b5a568884b1ae9d9fb7cbd905e1
William <william.michaux@gmail.com>
2018-01-29 21:32:28 +0100

    Initialisation du tableau + Ajout des bombes OK//Manque le nombre de bombes voisines pour chauque case car problème pour gérer les case en bord de carte. Idée à suivre utiliser modulo pour la partie de droite, comparer < 10 pour la première ligne, comparer < sqr(tailleCarte) - 2 pour la dernière ligne. Aucune idée pour la colonne de droite

--- js/demineur.js
+++ js/demineur.js
@@ -10,19 +10,19 @@ formElt.addEventListener("click", function (e) {
     var tailleCarte;
     var nbBombes;
     if (difficulte === "facile") {
-        tailleCarte = 10;
+        tailleCarte = 100;
         nbBombes = 10;
     } else if (difficulte === "moyen") {
-        tailleCarte = 15;
+        tailleCarte = 255;
         nbBombes = 50;
     } else {
-        tailleCarte = 20;
+        tailleCarte = 400;
         nbBombes = 100;
     }
 
     console.log(tailleCarte, nbBombes);
 
-    initTab(tailleCarte, nbBombes);
+    initCarte(tailleCarte, nbBombes);
 });
 
 //fonction pour obtenir un nombre aléatoire
@@ -30,3 +30,44 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+function compteNbBombes(carte, indice) {
+    var nbBombes = 0;
+    caseVoisines = //T'es ici connard
+    
+    for (var i = 0;i< 8;i++) {
+        if (carte[i].nbBombesVoisines == -1) {
+            nbBombes++;
+        }
+    }
+    return nbBombes;
+}
+
+
+function initCarte(tailleCarte, nbBombes) {
+    var carte = [];
+
+    for (var iCase = 0; iCase < tailleCarte; iCase++) {
+        var nouvelleCase = new Case(0, false);
+        carte.push(nouvelleCase);
+    }
+
+    var iBombe = 0;
+    while (iBombe < nbBombes) {
+        var randInt = getRandomInt(tailleCarte);
+        if (carte[randInt].nbBombesVoisines == 0) {
+            carte[randInt].nbBombesVoisines = -1;
+            iBombe++;
+        }
+        
+    }
+    for (var iCase = 0; iCase < tailleCarte; iCase++) {
+        if (carte[iCase].nbBombesVoisines == 0) {
+            var nbBombes = compteNbBombes(carte, iCase)
+            carte[iCase].nbBombesVoisines = nbBombes;
+        }
+    }
+    /* Afficher les valeurs des cases
+    for (var i = 0; i< tailleCarte; i++) {
+        console.log(i + "-->" + carte[i].nbBombesVoisines);
+    }*/
+}
